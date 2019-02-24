07dc9b610aa0b36bf6cfb6f779583e25576715ce
Brian Di Rito <bdirito91@gmail.com>
2019-02-24 22:46:49 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,9 @@
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER
-);
-
-CREATE TABLE pledges(
-  id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
-);
-
-CREATE TABLE projects (
+CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal REAL,
+  funding_goal INTEGER
   start_date TEXT,
   end_date TEXT
-);
+)
+
